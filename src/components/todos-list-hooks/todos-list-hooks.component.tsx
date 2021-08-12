34891bcd7c87f86a7da651c1d9d8775910a5187b
Prasetya Aji Baskara <prasetya.baskara@btpn.com>
2021-08-12 11:29:33 +0700

    memoized useSelector

--- src/components/todos-list-hooks/todos-list-hooks.component.tsx
+++ src/components/todos-list-hooks/todos-list-hooks.component.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { _useSelector } from '../../hooks/_useSelector';
+import { selectTodosList, selectTodosError, selectTodosLoading } from '../../redux/todos/todos.selector';
 import { useTodosAction } from '../../hooks/useTodosAction';
 import useFetchTodos from '../../hooks/useFetchTodos';
 
 const TodosListHooks = () => {
-  const { list, loading, error } = _useSelector(state => state.todos);
+  // without memoized
+  // const { list, loading, error } = _useSelector(state => state.todos);
+  
+  // with memoized
+  const list = _useSelector(selectTodosList);
+  const loading = _useSelector(selectTodosLoading);
+  const error = _useSelector(selectTodosError);
 
   // call fetchTodosList action
   useFetchTodos();
@@ -23,7 +30,7 @@ const TodosListHooks = () => {
       <p>loading = {loading ? 'True' : 'False'}</p>
       <p>error = {error ? error : 'No Error'}</p>
       <p>todos.list.length = {list.length}</p>
-      
+
       <button onClick={onClick}>fetchList</button>
     </div>
   )
